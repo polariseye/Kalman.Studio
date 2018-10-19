c6a02fcd0fa51442c0f01387857730da4e3941f8
liujun <polariseye@126.com>
2018-10-19 18:30:02 +0800

    完善代码修复配置不正确的bug

--- src/Kalman.Studio/T4Template/CSharp/Global/BLL_List.cs
+++ src/Kalman.Studio/T4Template/CSharp/Global/BLL_List.cs
@@ -97,6 +97,21 @@ namespace Kalman.Studio.T4Template.CSharp.Global
                     "****************");
             return this.GenerationEnvironment.ToString();
         }
+        private global::Microsoft.VisualStudio.TextTemplating.ITextTemplatingEngineHost hostValue;
+        /// <summary>
+        /// The current host for the text templating engine
+        /// </summary>
+        public virtual global::Microsoft.VisualStudio.TextTemplating.ITextTemplatingEngineHost Host
+        {
+            get
+            {
+                return this.hostValue;
+            }
+            set
+            {
+                this.hostValue = value;
+            }
+        }
     }
     
     #line default
